9547717cff6dfd7ba8263e30dffdcf2a3a8e1bf6
Anastasia <kaselyuskina74@gmail.com>
2023-07-20 23:41:12 +0500

    Смена кодировки

--- HW12 - Indexes/Indexes.sql
+++ HW12 - Indexes/Indexes.sql
@@ -1,16 +1,16 @@
 /*
-Возможно в ходе дальнейшей работы появятся новые индексы или будут редактироваться старые
-На текущий момент могу предположить к использованию следующие индексы
+Р’РѕР·РјРѕР¶РЅРѕ РІ С…РѕРґРµ РґР°Р»СЊРЅРµР№С€РµР№ СЂР°Р±РѕС‚С‹ РїРѕСЏРІСЏС‚СЃСЏ РЅРѕРІС‹Рµ РёРЅРґРµРєСЃС‹ РёР»Рё Р±СѓРґСѓС‚ СЂРµРґР°РєС‚РёСЂРѕРІР°С‚СЊСЃСЏ СЃС‚Р°СЂС‹Рµ
+РќР° С‚РµРєСѓС‰РёР№ РјРѕРјРµРЅС‚ РјРѕРіСѓ РїСЂРµРґРїРѕР»РѕР¶РёС‚СЊ Рє РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЋ СЃР»РµРґСѓСЋС‰РёРµ РёРЅРґРµРєСЃС‹
 */
 
-/*Получить всех дилеров по номеру и признаку удаления 0/1*/
+/*РџРѕР»СѓС‡РёС‚СЊ РІСЃРµС… РґРёР»РµСЂРѕРІ РїРѕ РЅРѕРјРµСЂСѓ Рё РїСЂРёР·РЅР°РєСѓ СѓРґР°Р»РµРЅРёСЏ 0/1*/
 CREATE NONCLUSTERED INDEX [IX_dealers_seq_number_is_deleted_incl_name] ON [Directory].[Dealers]
 (
 	[seq_number] ASC,
 	[is_deleted] ASC
 ) INCLUDE([name])
 
-/*Получить всех дилеров (открыт/закрыт контракт) по владельцу*/
+/*РџРѕР»СѓС‡РёС‚СЊ РІСЃРµС… РґРёР»РµСЂРѕРІ (РѕС‚РєСЂС‹С‚/Р·Р°РєСЂС‹С‚ РєРѕРЅС‚СЂР°РєС‚) РїРѕ РІР»Р°РґРµР»СЊС†Сѓ*/
 CREATE NONCLUSTERED INDEX [IX_contracts_owner_id_is_closed_incl_dealer_id] ON [Directory].[Contracts]
 (
 	[owner_id] ASC,
@@ -18,7 +18,7 @@ CREATE NONCLUSTERED INDEX [IX_contracts_owner_id_is_closed_incl_dealer_id] ON [D
 )
 INCLUDE([dealer_id])
 
-/*Получить всех владельцев по дилеру (открыт/закрыт контракт)*/
+/*РџРѕР»СѓС‡РёС‚СЊ РІСЃРµС… РІР»Р°РґРµР»СЊС†РµРІ РїРѕ РґРёР»РµСЂСѓ (РѕС‚РєСЂС‹С‚/Р·Р°РєСЂС‹С‚ РєРѕРЅС‚СЂР°РєС‚)*/
 CREATE NONCLUSTERED INDEX [IX_contracts_dealer_id_is_closed_incl_owner_id] ON [Directory].[Contracts]
 (
 	[dealer_id] ASC,
@@ -26,21 +26,21 @@ CREATE NONCLUSTERED INDEX [IX_contracts_dealer_id_is_closed_incl_owner_id] ON [D
 )
 INCLUDE([owner_id])
 
-/*Получить все мероприятия по владельцу (имя и номер)*/
+/*РџРѕР»СѓС‡РёС‚СЊ РІСЃРµ РјРµСЂРѕРїСЂРёСЏС‚РёСЏ РїРѕ РІР»Р°РґРµР»СЊС†Сѓ (РёРјСЏ Рё РЅРѕРјРµСЂ)*/
 CREATE NONCLUSTERED INDEX [IX_events_owner_id_incl_seq_number_name] ON [Directory].[Events]
 (
 	[owner_id] ASC
 )
 INCLUDE([seq_number],[name])
 
-/*Получить наименование мероприятия и владельца по номеру*/
+/*РџРѕР»СѓС‡РёС‚СЊ РЅР°РёРјРµРЅРѕРІР°РЅРёРµ РјРµСЂРѕРїСЂРёСЏС‚РёСЏ Рё РІР»Р°РґРµР»СЊС†Р° РїРѕ РЅРѕРјРµСЂСѓ*/
 CREATE NONCLUSTERED INDEX [IX_events_seq_number_incl_owner_id_name] ON [Directory].[Events]
 (
 	[seq_number] ASC
 )
 INCLUDE([name],[owner_id])
 
-/*получить все события по мероприятию с датой начала*/
+/*РїРѕР»СѓС‡РёС‚СЊ РІСЃРµ СЃРѕР±С‹С‚РёСЏ РїРѕ РјРµСЂРѕРїСЂРёСЏС‚РёСЋ СЃ РґР°С‚РѕР№ РЅР°С‡Р°Р»Р°*/
 CREATE NONCLUSTERED INDEX [IX_event_id_hall_id_incl_start_date] ON [Directory].[Events_time]
 (
 	[event_id] ASC,
@@ -48,7 +48,7 @@ CREATE NONCLUSTERED INDEX [IX_event_id_hall_id_incl_start_date] ON [Directory].[
 )
 INCLUDE([start_date])
 
-/*Получить все открытые мероприятия, которые будут проходить в определенном зале*/
+/*РџРѕР»СѓС‡РёС‚СЊ РІСЃРµ РѕС‚РєСЂС‹С‚С‹Рµ РјРµСЂРѕРїСЂРёСЏС‚РёСЏ, РєРѕС‚РѕСЂС‹Рµ Р±СѓРґСѓС‚ РїСЂРѕС…РѕРґРёС‚СЊ РІ РѕРїСЂРµРґРµР»РµРЅРЅРѕРј Р·Р°Р»Рµ*/
 CREATE NONCLUSTERED INDEX [IX_hall_id_incl_event_id_start_date] ON [Directory].[Events_time]
 (
 	[hall_id] ASC
@@ -56,51 +56,51 @@ CREATE NONCLUSTERED INDEX [IX_hall_id_incl_event_id_start_date] ON [Directory].[
 INCLUDE([event_id],[start_date]) 
 WHERE ([is_closed]=(0))
 
-/*получить наименования всех залов определенной площадки*/
+/*РїРѕР»СѓС‡РёС‚СЊ РЅР°РёРјРµРЅРѕРІР°РЅРёСЏ РІСЃРµС… Р·Р°Р»РѕРІ РѕРїСЂРµРґРµР»РµРЅРЅРѕР№ РїР»РѕС‰Р°РґРєРё*/
 CREATE NONCLUSTERED INDEX [IX_halls_venue_id_incl_name] ON [Directory].[Halls]
 (
 	[venue_id] ASC
 )
 INCLUDE([name])
 
-/*получить наименование владельца по номеру*/
+/*РїРѕР»СѓС‡РёС‚СЊ РЅР°РёРјРµРЅРѕРІР°РЅРёРµ РІР»Р°РґРµР»СЊС†Р° РїРѕ РЅРѕРјРµСЂСѓ*/
 CREATE NONCLUSTERED INDEX [IX_owners_seq_number_incl_name] ON [Directory].[Owners]
 (
 	[seq_number] ASC
 )
 INCLUDE([name])
 
-/*достать все места в определенном зале*/
+/*РґРѕСЃС‚Р°С‚СЊ РІСЃРµ РјРµСЃС‚Р° РІ РѕРїСЂРµРґРµР»РµРЅРЅРѕРј Р·Р°Р»Рµ*/
 CREATE NONCLUSTERED INDEX [IX_places_hall_id_incl_sector_row_number_place_number] ON [Directory].[Places]
 (
 	[hall_id] ASC
 )
 INCLUDE([sector],[row_number],[place_number])
 
-/*получить все точки продаж дилера*/
+/*РїРѕР»СѓС‡РёС‚СЊ РІСЃРµ С‚РѕС‡РєРё РїСЂРѕРґР°Р¶ РґРёР»РµСЂР°*/
 CREATE NONCLUSTERED INDEX [IX_dealer_id_incl_name] ON [Directory].[Points]
 (
 	[dealer_id] ASC
 )
 INCLUDE([name]) 
 
-/*получить все билеты по определенному статусу (как правило свободные билеты) и событию*/
+/*РїРѕР»СѓС‡РёС‚СЊ РІСЃРµ Р±РёР»РµС‚С‹ РїРѕ РѕРїСЂРµРґРµР»РµРЅРЅРѕРјСѓ СЃС‚Р°С‚СѓСЃСѓ (РєР°Рє РїСЂР°РІРёР»Рѕ СЃРІРѕР±РѕРґРЅС‹Рµ Р±РёР»РµС‚С‹) Рё СЃРѕР±С‹С‚РёСЋ*/
 CREATE NONCLUSTERED INDEX [IX_tickets_event_time_id_status_id_place_id_price] ON [Directory].[Tickets]
 (
 	[event_time_id] ASC,
 	[status_id] ASC
 )
 INCLUDE([place_id],[price])
---WHERE ([status_id]=(1000)) --1000 признак свободного билета, доступного к продаже. Не уверенная что фильтр нужен, возможно будет поиск по всем билетам.
+--WHERE ([status_id]=(1000)) --1000 РїСЂРёР·РЅР°Рє СЃРІРѕР±РѕРґРЅРѕРіРѕ Р±РёР»РµС‚Р°, РґРѕСЃС‚СѓРїРЅРѕРіРѕ Рє РїСЂРѕРґР°Р¶Рµ. РќРµ СѓРІРµСЂРµРЅРЅР°СЏ С‡С‚Рѕ С„РёР»СЊС‚СЂ РЅСѓР¶РµРЅ, РІРѕР·РјРѕР¶РЅРѕ Р±СѓРґРµС‚ РїРѕРёСЃРє РїРѕ РІСЃРµРј Р±РёР»РµС‚Р°Рј.
 
-/*Получить по номеру заказа его статус, сумму, дату заказа*/
+/*РџРѕР»СѓС‡РёС‚СЊ РїРѕ РЅРѕРјРµСЂСѓ Р·Р°РєР°Р·Р° РµРіРѕ СЃС‚Р°С‚СѓСЃ, СЃСѓРјРјСѓ, РґР°С‚Сѓ Р·Р°РєР°Р·Р°*/
 CREATE UNIQUE NONCLUSTERED INDEX [IX_seq_number_incl_amount_status_id_order_date] ON [Purchasing].[Orders]
 (
 	[seq_number] ASC
 )
 INCLUDE([amount],[status_id],[order_date])
 
-/*Получить по мероприятию и статусу номера заказов*/
+/*РџРѕР»СѓС‡РёС‚СЊ РїРѕ РјРµСЂРѕРїСЂРёСЏС‚РёСЋ Рё СЃС‚Р°С‚СѓСЃСѓ РЅРѕРјРµСЂР° Р·Р°РєР°Р·РѕРІ*/
 CREATE NONCLUSTERED INDEX [IX_event_time_id_status_id_incl_seq_number] ON [Purchasing].[Orders]
 (
 	[status_id] ASC,
@@ -108,7 +108,7 @@ CREATE NONCLUSTERED INDEX [IX_event_time_id_status_id_incl_seq_number] ON [Purch
 )
 INCLUDE([seq_number])
 
-/*По точке продаж и событию получить все номера заказов и их дату*/
+/*РџРѕ С‚РѕС‡РєРµ РїСЂРѕРґР°Р¶ Рё СЃРѕР±С‹С‚РёСЋ РїРѕР»СѓС‡РёС‚СЊ РІСЃРµ РЅРѕРјРµСЂР° Р·Р°РєР°Р·РѕРІ Рё РёС… РґР°С‚Сѓ*/
 CREATE NONCLUSTERED INDEX [IX_point_id_event_time_id_incl_seq_number] ON [Purchasing].[Orders]
 (
 	[point_id] ASC,
@@ -116,14 +116,14 @@ CREATE NONCLUSTERED INDEX [IX_point_id_event_time_id_incl_seq_number] ON [Purcha
 )
 INCLUDE([seq_number],[order_date])
 
-/*По заказу получить все билеты и их статус (статус так как билет может быть возвращен)*/
+/*РџРѕ Р·Р°РєР°Р·Сѓ РїРѕР»СѓС‡РёС‚СЊ РІСЃРµ Р±РёР»РµС‚С‹ Рё РёС… СЃС‚Р°С‚СѓСЃ (СЃС‚Р°С‚СѓСЃ С‚Р°Рє РєР°Рє Р±РёР»РµС‚ РјРѕР¶РµС‚ Р±С‹С‚СЊ РІРѕР·РІСЂР°С‰РµРЅ)*/
 CREATE NONCLUSTERED INDEX [IX_order_id_incl_ticket_id_status_id] ON [Purchasing].[Order_lines]
 (
 	[order_id] ASC
 )
 INCLUDE([ticket_id],[status_id])
 
-/*Получить все билеты и их статус по мероприятию и точке продаж*/
+/*РџРѕР»СѓС‡РёС‚СЊ РІСЃРµ Р±РёР»РµС‚С‹ Рё РёС… СЃС‚Р°С‚СѓСЃ РїРѕ РјРµСЂРѕРїСЂРёСЏС‚РёСЋ Рё С‚РѕС‡РєРµ РїСЂРѕРґР°Р¶*/
 CREATE NONCLUSTERED INDEX [IX_event_time_id_point_id_incl_ticket_id_status_id] ON [Purchasing].[Order_lines]
 (
 	[event_time_id] ASC,
@@ -131,7 +131,7 @@ CREATE NONCLUSTERED INDEX [IX_event_time_id_point_id_incl_ticket_id_status_id] O
 )
 INCLUDE([ticket_id],[status_id])
 
-/*получить сумму и дату транзакций по определенному заказу*/
+/*РїРѕР»СѓС‡РёС‚СЊ СЃСѓРјРјСѓ Рё РґР°С‚Сѓ С‚СЂР°РЅР·Р°РєС†РёР№ РїРѕ РѕРїСЂРµРґРµР»РµРЅРЅРѕРјСѓ Р·Р°РєР°Р·Сѓ*/
 CREATE NONCLUSTERED INDEX [IX_order_id_incl_transaction_amount_transaction_date] ON [Purchasing].[Order_transactions]
 (
 	[order_id] ASC
